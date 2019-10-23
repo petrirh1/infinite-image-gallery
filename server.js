2e241eaf783bec5efd803ebca3e8d9698fd91eb4
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2019-10-23 22:55:44 +0300

    initial commit

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
 const Unsplash = require('unsplash-js').default;
 const toJson = require('unsplash-js').toJson;
 const app = express();
 global.fetch = require('node-fetch');
 require('dotenv').config();
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 const unsplash = new Unsplash({
   accessKey: process.env.ACCESS_KEY,
@@ -25,6 +22,19 @@ app.get('/api/photos', (req, res) => {
     .then(json => res.json(json))
     .catch(err => {
       console.log(err);
-      res.sendStatus(500);
+      return res.sendStatus(500);
     });
 });
+
+// single page application
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
