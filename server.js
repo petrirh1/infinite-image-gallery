b0faf64de0754a7767d7cb0f0195d2fc0e9989e9
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2019-11-11 18:52:50 +0200

    tweaks

--- server.js
+++ server.js
@@ -29,19 +29,10 @@ app.get('/api/photos', (req, res) => {
 });
 
 // single page application
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static('client/build'));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// for deployment
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static('client/build'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + 'client/build/index.html'));
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
