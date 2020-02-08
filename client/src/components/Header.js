b1150d9fc1b31e5d8fdf6fd6520cdce173d19a34
petrirh1 <33935056+petrirh1@users.noreply.github.com>
2020-02-08 20:48:04 +0200

    First letter to uppercase

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,23 +5,21 @@ import Logo from './Logo';
 import './css/Header.css';
 
 const Header = () => {
-  const preferredTheme = useMediaPredicate('(prefers-color-scheme: dark)')
-    ? 'dark'
-    : 'light';
+	const preferredTheme = useMediaPredicate('(prefers-color-scheme: dark)') ? 'dark' : 'light';
 
-  return (
-    <Headroom disableInlineStyles={true} downTolerance={5} upTolerance={5}>
-      <header>
-        <Logo
-          className='app-logo'
-          width={20}
-          height={20}
-          fill={preferredTheme === 'dark' ? '#FFF' : '#212121'}
-        />
-        <h3 className='header-title'>infinite Gallery</h3>
-      </header>
-    </Headroom>
-  );
+	return (
+		<Headroom disableInlineStyles={true} downTolerance={5} upTolerance={5}>
+			<header>
+				<Logo
+					className='app-logo'
+					width={20}
+					height={20}
+					fill={preferredTheme === 'dark' ? '#FFF' : '#212121'}
+				/>
+				<h3 className='header-title'>Infinite Gallery</h3>
+			</header>
+		</Headroom>
+	);
 };
 
 export default Header;
